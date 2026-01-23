25b18f1902ead6d88d46b6719de0a760e58b283e
Aryan Mehta <aryanmehta2079@gmail.com>
2026-01-23 19:42:02 +0530

    Day-4 of C++ Programming

--- mergeSort/reversePair.cpp
+++ mergeSort/reversePair.cpp
@@ -1,10 +1,30 @@
 #include<iostream>
-#include<vector>
 using namespace std;
+// int reversePair(int arr[],int n){
+//     int count = 0;
+//     for(int i = 0;i<n;i++){
+//         for (int j = i+1;j<n;j++){
+//             if (arr[i]>2*arr[j]) count++;
+//         }
+//     }
+//     return count;
+// }
 
+int ans = 0;
 void merge(int arr[],int low,int mid,int high){
     int i = low;
     int j = mid+1;
+
+    while(i<=mid && j<=high){
+        if(arr[i]>2*arr[j]){
+            ans = ans + mid-i+1;
+            j++;
+        }
+        else i++;
+    }
+
+    i = low;
+    j = mid+1;
     int k = 0;
     int *result = new int[high-low+1];
     while(i<=mid && j<=high){
@@ -30,28 +50,24 @@ void merge(int arr[],int low,int mid,int high){
             j++;
     }
 
-    for(int i = 0;i<(high-low+1);i++){
-        arr[i+low] = result[i];
+    for(int i = low;i<=high;i++){
+        arr[i] = result[i-low];
     }
+    delete[] result;
 }
 
 void mergeSort(int arr[],int low,int high){
-    if(low<high){
+    if(low==high) return;
         int mid = (low+high)/2;
         mergeSort(arr,low,mid);
         mergeSort(arr,mid+1,high);
         merge(arr,low,mid,high);
-    }
+
 
 }
-
 int main(){
-    int arr[] = {5,1,1,6,2,0,0};
+    int arr[] = {1,3,2,3,1};
     int size = sizeof(arr)/sizeof(arr[0]);
-    // vector<int>nums(arr,arr+size);
-    // quickSort(arr);
     mergeSort(arr,0,size-1);
-    for(int i = 0;i<size;i++){
-        cout<<arr[i]<<" ";
-    }
+    cout<<ans;
 }
